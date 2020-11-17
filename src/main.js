c5a98abee2b1509554dd14f616ef1a0a680aa158
Jerrod Styrk <jstyrk@citadel.edu>
2020-11-17 14:41:52 -0800

    return a word to use for hangman
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,5 +4,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
-  event.preventDefault();
+  $('#start').click(function() {
+    $('#start').hide();
+    // let letter = $('.guess').val();
+
+    let promise = new Promise(function(resolve, reject) {
+      let word = new XMLHttpRequest();
+      const url = 'http://dinoipsum.herokuapp.com/api?format=json&paragraphs=1&words=1';
+
+      word.onload = function() {
+        if (this.status === 200) {
+          resolve(word.response);
+        } else {
+          reject(word.response);
+        }
+      };
+      word.open("GET", url, true);
+      word.send();
+    });
+
+    promise.then(function(response) {
+      const gameWord = JSON.parse(response);
+      console.log(`${gameWord[0][0]}`);
+      $('.guessed').text(`${gameWord[0][0]}`);
+    }, function(error) {
+      $('.guessed').text(`There was an error: ${error}`);
+    });
+  });
 });
